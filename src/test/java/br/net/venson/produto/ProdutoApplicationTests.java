dd3e3a95ca63977d7380b6e648ada65792a4df94
Kauan Fontanela <kauanfontanela09@gmail.com>
2024-04-20 16:38:39 -0300

    Remove unused code in ProdutoApplicationTests.java

--- src/test/java/br/net/venson/produto/ProdutoApplicationTests.java
+++ src/test/java/br/net/venson/produto/ProdutoApplicationTests.java
@@ -3,21 +3,9 @@ package br.net.venson.produto;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import br.net.venson.produto.models.Monster;
-import br.net.venson.produto.services.Open5eMonsterService;
-
 @SpringBootTest
 class ProdutoApplicationTests {
 	@Test
 	void contextLoads() {
-		Open5eMonsterService service = new Open5eMonsterService();
-		var monsters = service.getRandomMob(12, 1);
-
-		for (Monster monster : monsters) {
-			System.out.printf(
-					"Monster: %s, CR: %f\n",
-					monster.getName(),
-					monster.getCr());
-		}
 	}
 }
